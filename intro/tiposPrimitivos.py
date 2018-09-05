1793eb36584dd3bfcd8363d5b8287652a4ed67de
utech <utech.br@gmail.com>
2018-09-04 21:15:24 -0300

    tipos de dados

---
+++ intro/tiposPrimitivos.py
@@ -0,0 +1,15 @@
+#Tipos primitivos de dados
+#os tipos são int float bool e srt
+
+n = int(input('Digite um número'))
+m = int(input('Digite outro número'))
+soma = n+m
+
+print("A soma dos valores dá: ",soma)
+
+#trabalhando com bool = True ou False nesse padrão
+#str tem que estar entre aspas 'Oi' '4.5' '' etc
+print('A soma é {}'.format(soma))
+
+print('Os valores são {} e {} e a soma é {}'.format(n,m,soma))
+

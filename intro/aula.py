d882906e618a698eb581a3e84a7caad33b422ddb
utech <utech.br@gmail.com>
2018-09-08 00:09:17 -0300

    Áula até 19:55 do vídeo https://www.youtube.com/watch?v=a7DH88vk2Sk

---
+++ intro/aula.py
@@ -0,0 +1,26 @@
+a = str(input("Digite uma cadeia de caracteres\n"))
+#printando a string
+print(a)
+
+b = "Curso em Vídeo Python"
+#fatiando a palavra, vou printar do 9 até (21-1) pulando de 2 em 2
+print(b[9:21:2])
+# vai do inicio até o 5, como eu emiti ele começa do começo
+print(b[:5])
+#vai printar do 15 até o final
+print(b[15:])
+#vai printar do 9 até o final pulando 3 em 3
+print(b[9::3])
+#análise das strings - len
+print(len(b)) #tamanho(comprimeot da string)
+print(b.count('o')) #contando quantas letras 'o' tem na frase
+print(b.count('o', 0,13)) # indo do 0 -> (13-1) contando o número de 'o'
+print(b.find('deo')) #Mostra aonde começou (posição) da cadeia deo se retornar -1 é pq n existe
+print('Curso' in b)  #retorna true or false
+
+
+
+
+
+
+
